aeddc42f2bf8d47130e1aac81e62b1e4da4ab864
Fariz Armesta <fariz.armesta.id@gmail.com>
2025-11-28 13:11:07 +0700

    Refactor MainWindow UI setup and button creation
    Moved style and window setup to dedicated methods and introduced a create_button helper for consistent button creation. Simplified widget initialization and layout code for better readability and maintainability. Also renamed docx test files into a DOCX subdirectory.

--- app.py
+++ app.py
@@ -24,27 +24,18 @@ class MainWindow(QMainWindow):
             
     def __init__(self):
         super().__init__()
-        
-        with open("style.qss", "r") as f:
-            self.setStyleSheet(f.read())
+        self.setup_style()
+        self.window_setup()
+        self.setup_wdidgets()
         
         self.history = HistoryManager()
-        
-        self.setWindowTitle("File Sorter - Armesta")
-        self.setMinimumSize(400, 300)
-        self.setWindowIcon(QIcon("ARMESTA2_small.png"))
         
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         
         input_row = QHBoxLayout() 
-        
         layout = QVBoxLayout()
-        
         nav_layout = QVBoxLayout()
-        
-        self.btn_home = QPushButton("Home")
-        self.btn_second = QPushButton("Second Page")
         
         self.btn_home.clicked.connect(lambda: self.stack.setCurrentIndex(0))
         self.btn_second.clicked.connect(lambda: self.stack.setCurrentIndex(1))
@@ -63,25 +54,27 @@ class MainWindow(QMainWindow):
         self.input_box.setFixedSize(250, 35)
         self.input_box.setObjectName("input_box")
         
-        self.clear_button = QPushButton("X")
-        self.clear_button.setFixedSize(30, 30)
+        self.clear_button = self.create_button(
+            "X", "clear_button", height=30, width=30
+        )
         self.clear_button.clicked.connect(self.input_box.clear)
-        self.clear_button.setObjectName("clear_button")
+
         
         button_row = QHBoxLayout()
         
         self.label = QLabel("")
         
-        button = QPushButton("Clean Folder")
-        button.setFixedSize(150, 40)
-        button.setObjectName("clean_button")
+        button = self.create_button(
+            "Clean Folder", "clean_button"
+        )
         button.clicked.connect(self.show_text)
         
+        
         self.label_folder = QLabel("")
-        self.button_folder = QPushButton("Select Folder")
+        self.button_folder = self.create_button(
+            "Select Folder", "folder_button"
+        )
         self.button_folder.clicked.connect(self.select_folder)
-        self.button_folder.setFixedSize(150, 40)
-        self.button_folder.setObjectName("folder_button")
           
         self.table = QTableWidget()
         input_row.addWidget(self.input_box)
@@ -97,20 +90,45 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.label_folder)
         layout.addWidget(self.table)
         
-        history_button = QPushButton("Show History")
-        history_button.setObjectName("history_button")
-        history_button.clicked.connect(self.show_history)
-        layout.addWidget(history_button)
         
-        self.delete_button = QPushButton("Delete All History")
+        self.history_button = self.create_button(
+            "Show History", "history_button", no_size=True
+        )
+        self.history_button.clicked.connect(self.show_history)
+        
+        
+        layout.addWidget(self.history_button)
+        
+        self.delete_button = self.create_button(
+            "Delete All History", "delete_button"
+        )
         self.delete_button.clicked.connect(self.delete_history)
-        self.delete_button.setFixedSize(150, 40)
-        self.delete_button.setObjectName("delete_button")
         
         layout.addWidget(self.delete_button)
         
         central_widget.setLayout(layout)
         
+    def setup_style(self):
+        with open("style.qss", "r") as f:
+            self.setStyleSheet(f.read())
+        
+    def window_setup(self):
+        self.setWindowTitle("File Sorter - Armesta")
+        self.setMinimumSize(400, 300)
+        self.setWindowIcon(QIcon("ARMESTA2_small.png"))
+    
+    def create_button(self, text, obj_name, height=40, width=150, no_size=False):
+        button = QPushButton(text)
+        button.setObjectName(obj_name)
+        if no_size == False:
+            button.setFixedSize(width, height)
+            return button
+        else:
+            return button    
+        
+    def setup_wdidgets(self):
+        self.btn_home = QPushButton("Home")
+        self.btn_second = QPushButton("Second Page")
         
     def show_error(self, message):
         msg = QMessageBox()
@@ -120,7 +138,6 @@ class MainWindow(QMainWindow):
         msg.exec()
         self.input_box.clear()
         
-            
     def show_text(self):
         path = self.input_box.text()
         
