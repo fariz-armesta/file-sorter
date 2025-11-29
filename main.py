cecebade787d768cd06d9524df5c62971b27e01c
Fariz Armesta <fariz.armesta.id@gmail.com>
2025-11-29 19:45:51 +0700

    Rename icon file and update application icon setup
    Renamed ARMESTA2_small.ico to ARMESTA_small.ico and added Aris.ico. Updated main.py to set the Windows AppUserModelID for better taskbar icon support and removed explicit window icon setting.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtGui import QIcon
 
 from app import MainWindow
 
+import ctypes
 
 def main():
     """Main function to run the File Sorter application."""
+    
+    myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("ARMESTA2_small.ico"))
     
     window = MainWindow()
     window.show()
